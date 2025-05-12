f2fa6ae0bf80f1c917c45af0201eea38f37faa62
omar samir <omersamer120@gmail.com>
2025-05-12 23:31:40 +0300

    first commit

---
+++ src/main/java/com/email/writer/controllers/EmailGeneratorController.java
@@ -0,0 +1,28 @@
+package com.email.writer.controllers;
+
+import com.email.writer.models.EmailRequest;
+import com.email.writer.services.EmailService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/email")
+@CrossOrigin(origins = "*")
+public class EmailGeneratorController {
+
+    private final EmailService emailService;
+
+    public EmailGeneratorController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
+        String response = emailService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+
+
+}
