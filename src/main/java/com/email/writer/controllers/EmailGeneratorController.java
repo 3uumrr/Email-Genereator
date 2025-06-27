6814eb12313f31cbeefbf42d4b8fdbe5a9ce8725
omar samir <omersamer120@gmail.com>
2025-06-27 16:47:31 +0300

    create ApiResponse Class

--- src/main/java/com/email/writer/controllers/EmailGeneratorController.java
+++ src/main/java/com/email/writer/controllers/EmailGeneratorController.java
@@ -1,11 +1,16 @@
 package com.email.writer.controllers;
 
 import com.email.writer.models.EmailRequest;
+import com.email.writer.response.ApiResponse;
 import com.email.writer.services.EmailService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestController
 @RequestMapping("/api/email")
@@ -18,9 +23,13 @@ public class EmailGeneratorController {
     }
 
     @PostMapping("/generate")
-    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
-        String response = emailService.generateEmailReply(emailRequest);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<ApiResponse> generateEmail(@RequestBody EmailRequest emailRequest) {
+        try {
+            String response = emailService.generateEmailReply(emailRequest);
+            return ResponseEntity.ok(new ApiResponse("Success",response));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(),null));
+        }
     }
 
 
